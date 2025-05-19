1961918b172c796148a01f740215f77838e223e3
conettoovando <cris.onetto01@gmail.com>
2025-05-19 00:27:15 -0400

    aprendido los metodos de POO e inicio de interfaces

---
+++ Interfaces/src/Main.java
@@ -0,0 +1,8 @@
+/** las interfaces son una forma de implementar abstraccion, que es cuando ocultamos el detalle de como se realiza algo.
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
